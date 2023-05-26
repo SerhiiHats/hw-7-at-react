642e0b6ecaf034aa1972f1cdc294b01be2f977b6
Serhi Hats <gatsserv@gmail.com>
2023-05-26 21:20:18 +0300

    add devtools in homework

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,13 @@
-import {createStore} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import initialStore from "./initialStore";
 import reducer from "./rootReducer"
+import thunk from "redux-thunk";
 
 
-const store = createStore(reducer, initialStore);
+const store = createStore(
+  reducer,
+  initialStore,
+  compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ));
 
 export default store;
